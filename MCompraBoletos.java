c1ffb2d4e68e61bd42a0557a21e46a500a238e8e
zaidlex <sadx_1997_97728@live.com.mx>
2021-07-30 16:59:27 -0500

    Main del proyecto

---
+++ MCompraBoletos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mg.softwarecompradeboletosdecine;
+
+/**
+ *
+ * @author zchx
+ */
+public class MCompraBoletos {
+    
+    public static void main(String args[]){  
+        InterfazCartelera IC = new InterfazCartelera();
+    }  
+}
