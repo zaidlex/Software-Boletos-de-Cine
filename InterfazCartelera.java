932e2a48aeaa4240065009d8b80b2f4a6e96f797
Zaid Alejandro <sadx_1997_97728@live.com.mx>
2021-08-02 23:57:17 -0500

    Creando la interfaz básica

--- InterfazCartelera.java
+++ InterfazCartelera.java
@@ -1,17 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mg.softwarecompradeboletosdecine;
 
-/**
- *
- * @author zchx
- */
-public class InterfazCartelera {
+import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class InterfazCartelera extends JFrame {
     
-    public void InterfazCartelera(){
-       
+    private JButton BotonesCartelera;//botones para la cartelera
+    private JLabel NombreEmpresa;//Nombre de la empresa
+
+    public void InterfazCartelera(){//Constructor
+        setLayout(null);
+        startItems();
+        addCartelera();
+        addItems();
+    }
+
+    private void startItems(){
+        NombreEmpresa = new JLabel("Bienvenido a 'Nombre de la empresa'");
+        NombreEmpresa.setBounds(40,40,200,30);
+    }
+
+    public void addCartelera(){
+        //Aquí se creará los botones para cada película
+        BotonesCartelera = null;
+    }
+
+    public void addItems(){
+        add(NombreEmpresa);
+        //this.add(BotonesCartelera);
     }
 }
--- MCompraBoletos.java
+++ MCompraBoletos.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mg.softwarecompradeboletosdecine;
 
-/**
- *
- * @author zchx
- */
 public class MCompraBoletos {
     
     public static void main(String args[]){  
-        InterfazCartelera IC = new InterfazCartelera();
+        //Caracteristicas de la interfaz de la cartelera
+        int width = 1000;
+        int height = 700;
+
+        InterfazCartelera ICartelera = new InterfazCartelera();
+        ICartelera.setSize(width, height);
+		ICartelera.setVisible(true);
     }  
 }
